28bce9f3bd5ac1b17c7cab37c298da6e98950ff9
Nicu Nec <nnicu8@gmail.com>
2019-04-16 05:47:28 +0300

    Clase pentru fiecare tabel

---
+++ Player.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * User: Neculache Nicu
+ * Date: 14.04.2019
+ * Time: 18:20
+ */
+
+class Player
+{
+    private $id = 0;
+    private $id_table = 0;
+    private $name = "Player";
+    private $cards;
+
+    /**
+     * Player constructor.
+     * @param int $id
+     * @param int $id_table
+     * @param string $name
+     * @param array $cards
+     */
+    public function __construct($id, $id_table, $name, array $cards)
+    {
+        $this->id = $id;
+        $this->id_table = $id_table;
+        $this->name = $name;
+        $this->cards = $cards;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdTable()
+    {
+        return $this->id_table;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCards()
+    {
+        return $this->cards;
+    }
+
+    /**
+     * @param mixed $cards
+     */
+    public function setCards(array $cards)
+    {
+        $this->cards = $cards;
+    }
+}
