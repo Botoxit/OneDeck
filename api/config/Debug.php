c0675068ec003fd9d0f4388a3de8dc67b23bde58
Botoxit <nnicu8@gmail.com>
2019-08-12 22:23:01 +0300

    Adaugare clasa Debug

---
+++ api/config/Debug.php
@@ -0,0 +1,12 @@
+<?php
+
+class Debug
+{
+    public static function Log($message = "Unknown error message", $file = "", $level = "WARNING")
+    {
+        $date = date("Y-m-d h:m:s");
+        $message = "[$date][$file][$level] " . trim($message) . PHP_EOL;
+        $log_file = dirname(__DIR__) . '/log/' . date("Y-m-d") . '.log';
+        error_log($message, 3, $log_file);
+    }
+}
