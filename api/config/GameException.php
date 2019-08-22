e897e8a5c96e0d7206c8ffa74c48352d8a14e773
Nicu Nec <nnicu8@gmail.com>
2019-08-22 03:07:21 +0300

    Update GameException.php
    Cand arunci o exceptie GameException se va face o inregistrare in log file automat

--- api/config/GameException.php
+++ api/config/GameException.php
@@ -2,5 +2,9 @@
 
 class GameException extends Exception
 {
-
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        Debug::Log($code . ': ' . $message, parent::getFile(), 'EXCEPTION');
+        parent::__construct($message, $code, $previous);
+    }
 }
