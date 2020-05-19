1f68eedaa0abb70ecf7b872f0b4afec93ab2aca6
Botoxit <nnicu8@gmail.com>
2020-05-19 20:41:05 +0300

    Update cronJob.php
    Select name, players, create timestamp and round before delete

--- cronJob.php
+++ cronJob.php
@@ -5,17 +5,36 @@ include_once 'core/Database.php';
 $conn = Database::getConnection();
 date_default_timezone_set('Europe/Bucharest');
 
-$stmt = $conn->prepare("CALL `delete_inactive_tables`();");
 $message = "Log message";
+$date = date("Y-m-d H:i:s");
 
+$query = "SELECT t.id, t.name, t.players_limit, t.created_at, g.round FROM `game` g join `tables_list` t ON t.id = g.id where timestampdiff(SECOND,`change_at`,CURRENT_TIMESTAMP) > 300 AND g.id > 1";
+$stmt = $conn->prepare($query);
+if ($stmt->execute()) {
+    $table_list = [];
+    $result = $stmt->get_result();
+    if ($result->num_rows == 0)
+        die();
+    while ($row = $result->fetch_assoc()) {
+        $table_item = array(
+            "id" => $row['id'],
+            "tableName" => $row['name'],
+            "playersLimit" => $row['players_limit'],
+            "created_at" => $row['created_at'],
+            "round" => $row['round']
+        );
+        array_push($table_list, $table_item);
+    }
+    error_log("[$date] I selected following inactive tables: " . json_encode($table_list) . PHP_EOL, 3, "cronJob.log");
+} else $message = "I can't select inactive tables $stmt->errno: $stmt->error";
+
+$stmt = $conn->prepare("CALL `delete_inactive_tables`();");
 if ($stmt->execute()) {
     if ($stmt->affected_rows > 0) {
-        if(!$conn->commit())
+        if (!$conn->commit())
             $message = "Commit work failed, $conn->errno: $conn->error";
-        else $message = "I deleted " . $stmt->affected_rows/2 . " inactive tables.";
-    }
-    else die();
+        else die();
+    } else die();
 } else $message = "I can't delete inactive tables $stmt->errno: $stmt->error";
 
-$date = date("Y-m-d H:i:s");
 error_log("[$date] " . $message . PHP_EOL, 3, "cronJob.log");
